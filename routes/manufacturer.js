0792ae4a928e801776405c3f0d458370b620ae70
Dreaserous <dreaserous2.0@gmail.com>
2024-12-13 10:18:46 +0530

    Manufactured products upload testing #3

--- routes/manufacturer.js
+++ routes/manufacturer.js
@@ -24,8 +24,8 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
     const { rfid, plis, dop, doe } = req.body; // Assuming you're sending data in the body
-    console.log(rfid);
-    console.log(plis);
+    console.log(JSON.parse(rfid));
+    console.log(JSON.parse(plis));
     console.log(dop);
     console.log(doe);
 
