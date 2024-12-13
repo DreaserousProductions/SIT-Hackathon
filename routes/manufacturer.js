4f9af5f2334786cff581b65739a09229f6eb7015
Dreaserous <dreaserous2.0@gmail.com>
2024-12-13 10:47:59 +0530

    QRCode testing

--- routes/manufacturer.js
+++ routes/manufacturer.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { pool } = require('../server');
+const QRCode = require('qrcode');
 
 const router = express.Router();
 
@@ -24,7 +25,8 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
     const { rfid, plis, dop, doe } = req.body;
-    console.log(rfid.replaceAll(`'`, `"`), plis);
+
+    console.log(JSON.parse(rfid.replaceAll(`'`, `"`))["rfid1"]);
 
     pool.getConnection((err, connection) => {
         if (err) {
@@ -32,14 +34,28 @@ router.post('/', (req, res) => {
         }
 
         const query = 'INSERT INTO manufactured_products (RFID, PLIS, DOP, DOE) VALUES (?, ?, ?, ?);';
-        connection.query(query, [rfid.replaceAll(`'`, `"`), plis.replaceAll(`'`, `"`), dop, doe], (err, result) => {
+        connection.query(query, [rfid.replaceAll(`'`, `"`), plis.replaceAll(`'`, `"`), dop, doe], async (err, result) => {
             connection.release(); // Always release the connection
 
             if (err) {
                 return res.status(500).json({ message: 'Failed to insert data', error: err });
             }
 
-            res.status(200).json({ message: 'Data inserted successfully', result });
+            // res.status(200).json({ message: 'Data inserted successfully', result });
+            // try {
+            //     // Generate QR code as a buffer
+            //     const qrBuffer = await QRCode.toBuffer(, {
+            //         errorCorrectionLevel: 'H', // High error correction level
+            //         type: 'png',               // Output as PNG
+            //     });
+
+            //     // Set the content type to PNG
+            //     res.setHeader('Content-Type', 'image/png');
+            //     res.send(qrBuffer); // Send the PNG buffer as the response
+            // } catch (err) {
+            //     console.error('Error generating QR code:', err);
+            //     res.status(500).json({ error: 'Failed to generate QR code' });
+            // }
         });
     });
 });
