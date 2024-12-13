7e36d7b4f4348ebbadd55e50c35ce4932444d96f
Dreaserous <dreaserous2.0@gmail.com>
2024-12-13 17:39:59 +0530

    RFID App #cont1

--- routes/rfid_conn.js
+++ routes/rfid_conn.js
@@ -24,7 +24,6 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
     const { rfid, plis, loc, term } = req.body; // Assuming you're sending data in the body
-    console.log(plis.replaceAll(`'`, `"`));
 
     pool.getConnection((err, connection) => {
         if (err) {
@@ -41,7 +40,7 @@ router.post('/', (req, res) => {
                 }
 
                 if (result.length != 0) {
-                    connection.query(prevQuery, [rfid, plis.replaceAll(`'`, `"`), loc, result[0]["RLID"], term], (err, result) => {
+                    connection.query(prevQuery, [rfid, String(plis).replaceAll(`'`, `"`), loc, result[0]["RLID"], term], (err, result) => {
                         connection.release();
                         if (err) {
                             return res.status(500).json({ message: 'Failed to insert data', error: err });
@@ -54,7 +53,7 @@ router.post('/', (req, res) => {
                 res.status(200).json({ message: 'Data inserted successfully', result });
             });
         } else {
-            connection.query(newQuery, [rfid, plis.replaceAll(`'`, `"`), loc, term], (err, result) => {
+            connection.query(newQuery, [rfid, String(plis).replaceAll(`'`, `"`), loc, term], (err, result) => {
                 connection.release();
                 if (err) {
                     return res.status(500).json({ message: 'Failed to insert data', error: err });
