c152fdfe806caa4e63a0432c340d42f1ab9a959f
Dreaserous <dreaserous2.0@gmail.com>
2024-12-14 02:28:43 +0530

    Product details API

--- routes/product_details.js
+++ routes/product_details.js
@@ -3,10 +3,19 @@ const { pool } = require('../server');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    const { rfid } = req.query;
+function plisReader(plis) {
+    const prodID = Number(plis["start"].split("_")[0]);
 
-    if (!rfid) {
+    const start = Number(plis["start"].split("_")[1]);
+    const end = Number(plis["end"].split("_")[1]);
+
+    return { prodID, start, end };
+}
+
+router.get('/', (req, res) => {
+    const { ppid } = req.query;
+
+    if (!ppid) {
         return res.status(400).json({ error: 'Missing RFID parameter' });
     }
 
@@ -15,15 +24,25 @@ router.get('/', (req, res) => {
             return res.status(500).json({ message: 'Database connection failed', error: err });
         }
 
-        const query = 'SELECT * FROM manufactured_products WHERE JSON_CONTAINS(RFID, ?);';
-        connection.query(query, [rfid], (err, result) => {
+        const query = 'SELECT * FROM rfid_logs ORDER BY RLID DESC;';
+        connection.query(query, (err, result) => {
             connection.release(); // Always release the connection
-
             if (err) {
                 return res.status(500).json({ message: 'Failed to retrieve data', error: err });
             }
 
-            res.status(200).json({ message: 'Data successfully retrieved', result });
+            const prodNid = ppid.split("_");
+            const pid = Number(prodNid[0]);
+            const id = Number(prodNid[1]);
+
+            result.forEach(res => {
+                const { prodID, start, end } = plisReader(res["PLIS"])
+
+                if (prodID === pid) {
+                    if (id >= start && id <= end) { }
+                    return res.status(200).json({ message: 'Data successfully retrieved', res });
+                }
+            });
         });
     });
 });
