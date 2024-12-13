918eab7bb0fe1763a23844d995dceb36fcb2b8ff
Dreaserous <dreaserous2.0@gmail.com>
2024-12-13 18:56:16 +0530

    Reskeleton starting inv #11

--- routes/rfid_conn.js
+++ routes/rfid_conn.js
@@ -37,7 +37,7 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
     const { rfid, wid, plis, loc } = req.body; // Assuming you're sending data in the body
-    const jPlis = JSON.stringify(plis);
+    const jPlis = plis.replaceAll(`'`, `"`);
     console.log(jPlis);
 
     pool.getConnection((err, connection) => {
