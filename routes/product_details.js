11d8888fba6f4fab202c0b87d8432e88860f5ece
Dreaserous <dreaserous2.0@gmail.com>
2024-12-14 10:09:34 +0530

    Details#2241

--- routes/product_details.js
+++ routes/product_details.js
@@ -106,7 +106,7 @@ router.get('/', (req, res) => {
                                                     <li class="location-item">
                                                         <strong>Location:</strong> ${location.LOC}<br>
                                                         <span class="timestamp">Timestamp: ${new Date(location.TSTMP).toLocaleString()}</span><br>
-                                                        <strong>Temperature:</strong> ${warehouse}<br>
+                                                        <strong>Temperature:</strong> ${warehouse.TEMP}<br>
                                                         <strong>Humidity:</strong> ${warehouse.HUMIDITY}<br>
                                                     </li>
                                                 `).join('')}
