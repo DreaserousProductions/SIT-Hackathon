e1e80d266348347cbbfb3ccd3e0db657915695f7
Dreaserous <dreaserous2.0@gmail.com>
2024-12-14 10:05:45 +0530

    Details#2237

--- routes/product_details.js
+++ routes/product_details.js
@@ -55,6 +55,7 @@ router.get('/', (req, res) => {
                                         if (err) {
                                             return reject(err);
                                         }
+                                        console.log(warehouseResult);
                                         resolve({
                                             location,
                                             warehouse: warehouseResult[0] || { TEMP: 'N/A', HUMIDITY: 'N/A' }
@@ -66,7 +67,6 @@ router.get('/', (req, res) => {
                             Promise.all(warehouseConditionsPromises)
                                 .then(data => {
                                     connection.release(); // Always release the connection
-                                    const wsrt = String(warehouse);
                                     // Generate combined HTML response
                                     const htmlResponse = `
                                         <!DOCTYPE html>
@@ -105,7 +105,7 @@ router.get('/', (req, res) => {
                                                     <li class="location-item">
                                                         <strong>Location:</strong> ${location.LOC}<br>
                                                         <span class="timestamp">Timestamp: ${new Date(location.TSTMP).toLocaleString()}</span><br>
-                                                        <strong>Temperature:</strong> ${wsrt}<br>
+                                                        <strong>Temperature:</strong> ${warehouse}<br>
                                                         <strong>Humidity:</strong> ${warehouse.HUMIDITY}<br>
                                                     </li>
                                                 `).join('')}
